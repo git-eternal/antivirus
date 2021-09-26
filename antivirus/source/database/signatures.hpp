3b361e6a5537e720864a2d98fb7ea0619a04ff2e
eternity <45088542+EternalRift@users.noreply.github.com>
2021-09-26 07:19:01 -0700

    Update signatures.hpp

--- antivirus/source/database/signatures.hpp
+++ antivirus/source/database/signatures.hpp
@@ -5,11 +5,6 @@
 #include <urlmon.h>
 #pragma comment(lib, "urlmon.lib")
 
-/*
-1. create a way to download all our yara files (maybe an update signatures button too)
-2. store the yara files in some cache somewhere, or in memory ? idk
-*/
-
 class Signatures
 {
 private:
